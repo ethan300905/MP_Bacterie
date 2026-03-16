1044640985ac9b7d2c38c280ebba8213d0b3a841
Ethan Bello <ethan.bello@epfl.ch>
2026-03-16 16:54:49 +0100

    Config projet

---
+++ partie1/src/Random/RandomGenerator.cpp
@@ -0,0 +1,20 @@
+/*
+ * POOSV 2026
+ * Marco Antognini & Jamila Sam
+ */
+
+#include <Random/RandomGenerator.hpp>
+
+std::mt19937& getRandomGenerator()
+{
+    static std::mt19937 algo;
+
+    static bool initialise = true;
+    if (initialise) {
+        initialise = false;
+        std::random_device rd;
+        algo.seed(rd());
+    }
+
+    return algo;
+}
