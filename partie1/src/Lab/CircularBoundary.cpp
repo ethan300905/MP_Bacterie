b6201a8fd229dc58049d796550f9512de4eccaae
Ethan Bello <ethan.bello@epfl.ch>
2026-03-17 11:07:43 +0100

    Test

---
+++ partie1/src/Lab/CircularBoundary.cpp
@@ -0,0 +1,8 @@
+/*
+ * POOSV 2026
+ * authors : ....
+ */
+
+#include <Lab/CircularBoundary.hpp>
+
+// CODE  TO BE COMPLETED
---
+++ partie1/src/Lab/CircularBoundary.hpp
@@ -0,0 +1,24 @@
+/*
+ * Microbial cultures 2026
+ * authors = ....
+ */
+
+#pragma once
+
+#include <Utility/Vec2d.hpp>
+#include <ostream>
+
+// CODE  TO BE COMPLETED
+
+/**
+ *  @brief A circular boundary is defined by a radius and a position
+ *
+ *  @note radius must be >= 0.
+ */
+class CircularBoundary
+{
+
+};
+
+// CODE  TO BE COMPLETED
+
