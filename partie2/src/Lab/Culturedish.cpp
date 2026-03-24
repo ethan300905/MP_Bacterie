1092d93949e69d2abe2518fb683e4508d43c3230
Gab <gabriel.dubault@epfl.ch>
2026-03-24 11:04:42 +0100

    test echange données

--- partie2/src/Lab/Culturedish.cpp
+++ partie2/src/Lab/Culturedish.cpp
@@ -7,3 +7,20 @@ CultureDish::CultureDish()
 
 }
 
+//(méthode)faire évoluer les bactéries ici:
+//le type bool permet de savoir si la bactérie ou le nutriment a bien été ajouté
+bool addBacterium(Bacterium*){
+
+}
+bool addNutrient(Nutrient*){
+
+}
+void update(sf::Time dt){
+
+}
+void drawOn(sf::RenderTarget& targetWindow){  //pas encore sur du void
+
+}
+
+// AKLJFHLEOIFHöLDKSNFöLAKSJDH:ALKSNFKJNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+//fawegfoaqjwdbglakwuhfawrg
