d85591e2b5b95a3b364b1e2a9fec5973adcfb4a7
Gab <gabriel.dubault@epfl.ch>
2026-03-24 10:31:04 +0100

    test stash

--- partie2/src/Lab/Culturedish.hpp
+++ partie2/src/Lab/Culturedish.hpp
@@ -3,17 +3,33 @@
 #include "Bacterium.hpp"
 #include "Nutrient.hpp"
 #include <vector>
+#include <SFML/Graphics.hpp>
+#include "Application.hpp" //a voir quelles inclusions sont nécéssaires
 
 class CultureDish
 {
 public:
     CultureDish();
+    //(méthode)faire évoluer les bactéries ici:
+    //le type bool permet de savoir si la bactérie ou le nutriment a bien été ajouté
+    bool addBacterium(Bacterium*){
+
+    }
+    bool addNutrient(Nutrient*){
+
+    }
+    void update(sf::Time dt){
+
+    }
+    void drawOn(sf::RenderTarget& targetWindow){  //pas encore sur du void
+
+    }
+
 
 private:
-    vector<Bacterium> Bacteriums_;
-    vector<Nutrient> Nutrientsource_;
+    std::vector<Bacterium> Bacteriums_;
+    std::vector<Nutrient> Nutrientsource_;
     double temperature_;
-}
 };
 
 #endif // CULTUREDISH_HPP
