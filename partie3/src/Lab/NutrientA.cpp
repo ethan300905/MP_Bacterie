3b4681188a0bc7458bd94e099d1134e63043e806
ethan300905 <ethanmichaelbello@gmail.com>
2026-04-06 22:26:32 +0200

    Simplify NutrientA constructor and clean up code
    Refactor NutrientA constructor for brevity and remove commented-out methods.

--- partie3/src/Lab/NutrientA.cpp
+++ partie3/src/Lab/NutrientA.cpp
@@ -2,18 +2,8 @@
 #include "Application.hpp"
 
 NutrientA::NutrientA(const Quantity& quantity, Vec2d positionNutrient)
-    : Nutrient(quantity, positionNutrient)
-{
+    : Nutrient(quantity, positionNutrient) {}
 
-}
-/*
-void NutrientA::drawOn(sf::RenderTarget& target) const{
-    Nutrient::drawOn(target);
-}
-
-void NutrientA::update(sf::Time dt){
-    Nutrient::update(dt);
-}*/
 j::Value const& NutrientA::getConfig() const{
 
     return getAppConfig()["nutrients"]["A"];
