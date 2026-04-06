f220541c4e9f07e4128da5f6f257b21d4b5a511f
ethan300905 <ethanmichaelbello@gmail.com>
2026-04-06 22:27:29 +0200

    Clean up NutrientB.hpp by removing unused methods
    Removed commented-out update and drawOn methods.

--- partie3/src/Lab/NutrientB.hpp
+++ partie3/src/Lab/NutrientB.hpp
@@ -7,8 +7,7 @@ class NutrientB: public Nutrient
 {
 public:
     NutrientB(const Quantity&, Vec2d);
-    //void update(sf::Time) override;
-   // void drawOn(sf::RenderTarget& target) const override;
+
     j::Value const& getConfig() const override;
 };
 
